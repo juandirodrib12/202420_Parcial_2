4362d3cba8e2b2081eb0207e1d2471a6f5aa6157
juandirodrib12 <141871531+juandirodrib12@users.noreply.github.com>
2024-11-22 07:05:38 -0500

    ruta lista

--- pokemon/src/app/app.module.ts
+++ pokemon/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { TrainerModule } from './trainer/trainer.module';
+import { TrainerRoutingModule } from './trainer/trainer-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PokemonModule, TrainerModule],
+  imports: [BrowserModule, AppRoutingModule, PokemonModule, TrainerModule, TrainerRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
