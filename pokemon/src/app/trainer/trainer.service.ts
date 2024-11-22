586cb02bc4da023be4944dd125f2b17b3dbd92b2
juandirodrib12 <141871531+juandirodrib12@users.noreply.github.com>
2024-11-22 06:59:11 -0500

    service listo

---
+++ pokemon/src/app/trainer/trainer.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Trainer } from './Trainer';
+import { environment } from '../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TrainerService {
+
+private apiUrl = environment.baseUrl + 'trainers.json';
+
+constructor(private http: HttpClient) { }
+
+getTrainers(): Observable<Trainer[]> {
+  return this.http.get<Trainer[]>(this.apiUrl);
+}
+
+getDetailTrainer(id: number): Observable<Trainer> {
+  return this.http.get<Trainer>(this.apiUrl + id + '.json');
+}
+
+}
--- pokemon/src/environments/environment.development.ts
+++ pokemon/src/environments/environment.development.ts
@@ -1,1 +1,4 @@
-export const environment = {};
+export const environment = {
+    production: false,
+    baseUrl: 'https://raw.githubusercontent.com/2603-Uniandes/jsons/refs/heads/main/2024-20%20Pokemon/'
+};
